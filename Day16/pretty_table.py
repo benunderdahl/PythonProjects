ee7cb71368bd7db0e172162139e8eaa0f19d61a8
benunderdahl <benjamin.underdahl@proton.me>
2025-08-18 23:18:19 -0500

    testing git branch change

--- Day16/pretty_table.py
+++ Day16/pretty_table.py
@@ -4,5 +4,5 @@ table = prettytable.PrettyTable()
 
 table.add_column("Pokemon", ["Pikachu", "Charmander", "Squirtle"])
 table.add_column("Type", ["Electric", "Fire", "Water"])
-table.align = "l"
+
 print(table)
