7ec435474078283fdb6ccd2ac23ef354cb20c58b
benunderdahl <benjamin.underdahl@proton.me>
2025-08-22 17:29:20 -0500

    main.py added for snake game

---
+++ Day20/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Legend")
+
+
+positions = [(0, 0), (-20,0), (-40,0)]
+segments = []
+game = True
+
+for position in positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+while game:
+    for segment in segments:
+        segment.forward(10)
+
+
+screen.exitonclick()
