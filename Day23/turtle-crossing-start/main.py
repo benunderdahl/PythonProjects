01aff3d39918248417013f6a742d60a65b69ca09
benunderdahl <benjamin.underdahl@proton.me>
2025-08-25 09:27:28 -0500

    finished the turtle crossing game

--- Day23/turtle-crossing-start/main.py
+++ Day23/turtle-crossing-start/main.py
@@ -11,10 +11,26 @@ screen.listen()
 
 player = Player()
 score = Scoreboard()
+cars = CarManager()
 
 screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
+    cars.create_car()
+    cars.move_cars()
+    if player.ycor() > 280:
+        score.update_score()
+        cars.increase_speed()
+        player.reset()
+
+    for car in cars.all_cars:
+        if player.distance(car) < 25:
+            score.game_over()
+            game_is_on = False
+
+
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
--- Day23/turtle-crossing-start/scoreboard.py
+++ Day23/turtle-crossing-start/scoreboard.py
@@ -7,11 +7,17 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
-        self.level = 1
-        self.goto(0, 280)
+        self.level = 0
+        self.hideturtle()
+        self.goto(-210, 260)
         self.update_score()
 
     def update_score(self):
-        global FONT
-        self.score += 1
-        self.write(f"Level: {self.level}", align="left", FONT)
+        self.level += 1
+        self.clear()
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def game_over(self):
+        self.clear()
+        self.goto(0,0)
+        self.write("Game Over", align="center", font=FONT)
