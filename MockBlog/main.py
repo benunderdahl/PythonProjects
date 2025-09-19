651199c11456eb1dfb8a7954d9b1fc2006811d29
benunderdahl <benjamin.underdahl@proton.me>
2025-09-18 22:06:25 -0500

    updated blog them with bootstrap styling and added routed for a tags

--- MockBlog/main.py
+++ MockBlog/main.py
@@ -3,21 +3,24 @@ import requests
 
 
 app = Flask(__name__)
-api_endpoint = "https://api.npoint.io/c790b4d5cab58020d391"
-response = requests.get(api_endpoint)
-data = response.json()
-blogs = [item for item in data]
+
 
 @app.route('/')
 def home():
-    return render_template("index.html", blogs=blogs)
+    return render_template("index.html")
 
-@app.route("/post/<int:id>")
-def post(id):
-    for blog in blogs:
-        if blog['id'] == id:
-            post = blog
-    return render_template("post.html", post=post)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
